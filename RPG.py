b5a183dbe1c106f1331a87d3da7ef3e404365b93
The Silver Nova <48945006+ZeSilverNova@users.noreply.github.com>
2022-12-22 18:33:43 +0100

    Add files via upload

--- RPG.py
+++ RPG.py
@@ -6,17 +6,16 @@ print(" ")
 
 input("Please be aware that pressing the enter button without entering the recquired answers might result in a crash.")
 input("Entering letters instead of numbers may pose problems later, as you progress through the story.")
-input("The game is also case-sensitive, which mean that answers should be written in lower or higher cases accordingly." )
-input("Finally, there are no saves since I don't know how to code that yet." + "\033[0m")
+input("The game is also case-sensitive, which mean that answers should be written in lower or higher cases accordingly." + "\033[0m")
 print(" ")
 
 
 input("This is an experimental project I made during my free time. Might finish it someday, might not... Who knows.")
-input("For now at least I do hope you'll enjoy the adventure.")
+input("For now at least I hope you'll enjoy the adventure.")
 print(" ")
 
 
-input("Before we start, let me ask you a few questions.")
+input("Before we start, let's quickly make acquintance.")
 
 Player_name = input("\033[1;35m\n" + "What is your name ? ")
 Name_question = input("\033[1;35m\n" + "Is " + Player_name + " the name you choosed ?" + "\033[1;36m\n" + "[Y]  " + "  [N] ")
@@ -27,24 +26,13 @@ while Name_question != str("Y"):
             Player_name = input("\033[1;35m\n" + "Let me ask you this once again. What is your name ? ")
             Name_question = input("\033[1;35m\n" + "Is " + Player_name + " the name you choosed ?" + "\033[1;36m\n" + "[Y]  " + "  [N] ")
 else:
-    input("\033[1;37m\n" + "You will be remembered as " + Player_name + ".")
-
-
-Player_age = input("\033[1;35m\n" + "How old is " + Player_name + " ? ")
-Age_question = input("\033[1;35m\n" + "Is " + Player_name + " " + Player_age + " years old ?" + "\033[1;36m\n" + "[Y]  " + "  [N] ")
-Age_answer = Age_question + str("Y")
-while Age_question != str("Y"):
-    if Age_question != str("Y"):
-        if Age_answer != str("Y"):
-            Player_age = input("\033[1;35m\n" + "Let me ask you this once again. How old is " + Player_name + " ? ")
-            Age_question = input("\033[1;35m\n" + "Is " + Player_name + " " + Player_age + " years old ?" + "\033[1;36m\n" + "[Y]  " + "  [N] ")
-else:
-    input("\033[1;37m\n" + Player_name + " is " + Player_age + " years old." + "\033[0m")
+    input("\033[1;37m\n" + "You will be remembered as " + Player_name + "." + "\033[0m")
 
 print(" ")
 
 
-input(Player_name + ". Your adventure is about to start.")
+input("I have no more questions for you " + Player_name + ".")
+input("Your new adventure is about to start.")
 input("Trust your judgement, follow your instinct and have fun !")
 print(" ")
 
@@ -54,7 +42,7 @@ print("\033[1;37m\n" + "Initializing story, please be patient.")
 import time
 my_list = ['.', '.', '.', '.', '.']
 for i in my_list:
-    time.sleep(1)
+    time.sleep(0.5)
     print(i)
 
 
@@ -68,5 +56,6 @@ input("You knew the whole crew was condemned, and so you promptly left with the 
 print(" ")
 
 input("Minutes only after piloting the shuttle, you feel a tremendous impact.")
-input("You quickly glance back to where you came from and your eyes fill with dread as you see the mothership exploding and the pirates following you.")
-input(" ")
+input("You quickly glance back to where you came from.")
+input("Your eyes fill with dread as you see the mothership exploding into tiny pieces and the pirates following you.")
+print(" ")
